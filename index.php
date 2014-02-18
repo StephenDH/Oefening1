9a0d268b0dc75369a25e22493f11e287c5204310
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-02-18 11:56:13 +0100

    Added form html-code

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 
 <html>
 <head>
-	<title></title>
+	<title>Oefening PHP op POST en GET</title>
 </head>
 <body>
-
+	<h1>Regestratie</h1>
+	<p>Naam: <input type="text" name="real_name"/></p>
+	<p>Gebruikersnaam: <input type="text" name="user_name"/></p>
+	<p>Wachtwoord: <input type="password" name="password"/></p>
 </body>
 </html>
