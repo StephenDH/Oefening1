1a31044cbd2dd0299496aed77f634e576dff6dd0
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-02-23 14:43:50 +0100

    format code

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 	$real_name = $_GET['real_name'];
 	$user_name = $_GET['user_name'];
-	$password  = $_GET['password' ];
-	
+	$password  = $_GET['password' ];	
 ?>
-<!DOCTYPE html>
 
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Oefening PHP op POST en GET</title>
--- result.php
+++ result.php
@@ -1,6 +1,6 @@
 <?php  ?>
-<!DOCTYPE html>
 
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Resultaat</title>
