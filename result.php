9cc63cc783b718c9af053aff4de58c5eb20dd4bd
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-02-23 15:16:18 +0100

    Layout done

--- result.php
+++ result.php
@@ -4,9 +4,12 @@
 <html>
 <head>
 	<title>Resultaat</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
-	<h1>Welkom <?php echo $_GET["real_name"] ?></h1>
-	<p>Je gebruikersnaam is <?php echo $_GET["user_name"] ?></p>
+	<div class="jumbotron">
+		<h1>Welkom <?php echo $_GET["real_name"] ?></h1>
+		<p>Je gebruikersnaam is <?php echo $_GET["user_name"] ?></p>
+	</div>
 </body>
 </html>
