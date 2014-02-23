376f58b1b47267de411d988c2c2273c55cab36c8
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-02-23 14:30:14 +0100

    form doorsturen naar result.php.

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
-<?php  ?>
-<!DOCTYPE html>
+<?php
+	$real_name = $_POST['real_name'];
+	$user_name = $_POST['user_name'];
+	$password  = $_POST['password' ];
+?>
 
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Oefening PHP op POST en GET</title>
 </head>
 <body>
 	<h1>Regestratie</h1>
-	<p>Naam: <input type="text" name="real_name"/></p>
-	<p>Gebruikersnaam: <input type="text" name="user_name"/></p>
-	<p>Wachtwoord: <input type="password" name="password"/></p>
+
+	<form action="result.php" method="post">
+		<p>Naam: 		   <input type="text" 	  name="real_name" /></p>
+		<p>Gebruikersnaam: <input type="text" 	  name="user_name" /></p>
+		<p>Wachtwoord: 	   <input type="password" name="password"  /></p>
+		<input type="submit">
+	</form>
+
 </body>
 </html>
--- result.php
+++ result.php
@@ -1,11 +1,12 @@
 <?php  ?>
-<!DOCTYPE html>
 
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Resultaat</title>
 </head>
 <body>
-
+	<h1>Welkom <?php echo $_POST["real_name"] ?></h1>
+	<p>Je gebruikersnaam is <?php echo $_POST["user_name"] ?></p>
 </body>
 </html>
