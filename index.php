39967f0fd33fa6fff89fd16c40df7fd0bf99a84a
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-03-10 11:10:30 +0100

    Removed result.php and set the result in index.php. Fix typo

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,21 +5,27 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h1 class="panel-title">Regestratie</h1>
-		</div>
+	<?php if(isset($_POST['real_name']) && isset($_POST['user_name']) && isset($_POST['password'])) : ?>
+			<div class="jumbotron">
+				<h1>Welkom <?php echo $_POST["real_name"] ?></h1>
+				<p>Je gebruikersnaam is <?php echo $_POST["user_name"] ?></p>
+			</div>
 
-		<div class="panel-body">
-			<form action="result.php" method="post">
-				<p>Naam: 		   <input type="text" 	  name="real_name" class="form-control"/></p>
-				<p>Gebruikersnaam: <input type="text" 	  name="user_name" class="form-control"/></p>
-				<p>Wachtwoord: 	   <input type="password" name="password"  class="form-control"/></p>
-				<input type="submit" class="btn btn-default">
-			</form>			
-		</div>
-	</div>
+	<?php else : ?>	
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h1 class="panel-title">Registratie</h1>
+				</div>
 
-
+				<div class="panel-body">
+					<form method="post">
+						<p>Naam: 		   <input type="text" 	  name="real_name" class="form-control"/></p>
+						<p>Gebruikersnaam: <input type="text" 	  name="user_name" class="form-control"/></p>
+						<p>Wachtwoord: 	   <input type="password" name="password"  class="form-control"/></p>
+						<input type="submit" class="btn btn-default">
+					</form>			
+				</div>
+			</div>
+	<?php endif; ?>
 </body>
 </html>
